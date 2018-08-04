195bf3a76532e4da591b5641b9d40be74fad79d7
yeweili <yeweili94@gmail.com>
2018-08-04 12:06:34 +0800

    remove strategy directory file

--- strategy/flybehavior.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEAD_FIRST_DESIGN_PATTERN_STRATEGY
-#define HEAD_FIRST_DESIGN_PATTERN_STRATEGY
-
-class FlyBehavior {
-    virtual void fly() = 0;
-};
-#endif
--- strategy/quackbehavior.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEAD_FIRST_DESIGN_PATTERN_STRATEGY
-#define HEAD_FIRST_DESIGN_PATTERN_STRATEGY
-
-class Quack {
-    virtual void quack() = 0;
-};
-
-#endif
