28ebfdd636421abb744de710a462348ceb2d1121
yeweili94 <yeweili94@gmail.com>
2018-11-23 16:55:40 +0800

    fix buf

---
+++ observer_pattern/observer.h
@@ -0,0 +1,18 @@
+class Observer;
+class DisplayElement;
+class Subject {
+public:
+    virtual void registerObserver(Observer *o) = 0;
+    virtual void removeObserver(Observer *o) = 0;
+    virtual void notifyObservers() = 0;
+};
+
+class Observer {
+public:
+    virtual void update(float temp, float humidity, float pressure) = 0;
+};
+
+class DisplayElement {
+public:
+    virtual void display() = 0;
+};
